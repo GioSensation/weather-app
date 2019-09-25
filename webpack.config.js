7b89bff7a8f2ef5c9ae547a73ba154a8066ec689
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2019-09-25 14:57:25 +0200

    Add polyfills and preset-stage-0
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry  : './src/main.js',
+    entry  : ['babel-polyfill', './src/main.js'],
     output : {
         path     : __dirname,
         filename : 'weatherapp.dist.js'
@@ -12,6 +12,7 @@ module.exports = {
                 query: {
                     presets: [
                         'es2015',
+                        'stage-0',
                         'stage-2'
                     ]
                 }
